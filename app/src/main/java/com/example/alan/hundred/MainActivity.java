24f72e2cf121a9a4ad0fe97f98b1cb9d35cc08d5
alan <swd-004@mail.foxconn.com>
2018-01-08 14:53:47 +0800

    git push

--- app/src/main/java/com/example/alan/hundred/MainActivity.java
+++ app/src/main/java/com/example/alan/hundred/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.alan.hundred;
 
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.view.KeyEvent;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.FrameLayout;
 
 import com.example.alan.hundred.base.RxBaseActivity;
@@ -13,7 +17,7 @@ import butterknife.BindView;
  * @author Alan
  */
 
-public  class MainActivity extends RxBaseActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends RxBaseActivity implements NavigationView.OnNavigationItemSelectedListener {
 
 
     @BindView(R.id.fl_container)
@@ -22,6 +26,11 @@ public  class MainActivity extends RxBaseActivity implements NavigationView.OnNa
     @BindView(R.id.navigation_view)
     NavigationView navigationView;
 
+    @BindView(R.id.drawer_layout)
+    DrawerLayout mDrawerLayout;
+
+    private ActionBarDrawerToggle mDrawerToggle;
+
 
     @Override
     public int getLayoutId() {
@@ -29,6 +38,31 @@ public  class MainActivity extends RxBaseActivity implements NavigationView.OnNa
     }
 
 
+    @Override
+    protected void initView() {
+        super.initView();
+        initNavigationView();
+
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close) {
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+
+            }
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+
+            }
+        };
+        mDrawerToggle.syncState();
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        
+    }
+
+    private void initNavigationView() {
+        View viewHeader = navigationView.getHeaderView(0);
+    }
 
     @Override
     public void initToolBar() {
@@ -42,9 +76,28 @@ public  class MainActivity extends RxBaseActivity implements NavigationView.OnNa
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()){
-            case R.id.item_sigle_1:
+
+        switch (item.getItemId()) {
+            case R.id.item_single_1:
+                break;
+            default:
+                break;
         }
         return false;
+    }
+
+    /**
+     * 监听back键处理DrawerLayout和SearchView
+     */
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            if (mDrawerLayout.isDrawerOpen(mDrawerLayout.getChildAt(1))) {
+                mDrawerLayout.closeDrawers();
+            } else {
+
+            }
+        }
+        return true;
     }
 }
